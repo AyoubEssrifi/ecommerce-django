677ed505f401d2508f87ba18af1c95044d968dd9
AyoubEssrifi <essrifiayoub@gmail.com>
2021-04-14 11:33:40 +0200

    add REST api + authentication setup

--- ecommerce_website/ecommerce_website/settings.py
+++ ecommerce_website/ecommerce_website/settings.py
@@ -37,17 +37,27 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+    'djoser',
     'frontend'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000"    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'ecommerce_website.urls'
